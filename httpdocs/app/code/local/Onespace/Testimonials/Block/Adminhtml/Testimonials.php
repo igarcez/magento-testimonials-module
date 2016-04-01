73f6d5e783fe49bfa403e4f666fcd6d6d907eff1
Ian Garcez <ian@onespace.com.br>
2016-04-01 17:40:34 -0300

    add update and delete testimonial functions

--- httpdocs/app/code/local/Onespace/Testimonials/Block/Adminhtml/Testimonials.php
+++ httpdocs/app/code/local/Onespace/Testimonials/Block/Adminhtml/Testimonials.php
@@ -1,11 +1,15 @@
 <?php
 class Onespace_Testimonials_Block_Adminhtml_Testimonials extends Mage_Adminhtml_Block_Widget_Grid_Container {
+  public function __construct() {
+    parent::__construct();
+    $this->_removeButton('add');
+  }
+
   protected function _construct()
   {
     parent::_construct();
     $this->_blockGroup = 'testimonials';
     $this->_controller = 'adminhtml_testimonial';
     $this->_headerText = Mage::helper('testimonials')->__('Testimonials');
-    $this->_removeButton('add');
   }
 }
--- httpdocs/app/code/local/Onespace/Testimonials/controllers/Adminhtml/TestimonialsController.php
+++ httpdocs/app/code/local/Onespace/Testimonials/controllers/Adminhtml/TestimonialsController.php
@@ -13,10 +13,42 @@ class Onespace_Testimonials_Adminhtml_TestimonialsController extends Mage_Adminh
   {
     $testimonial = Mage::getModel('testimonials/testimonial');
     $testimonial->load($this->getRequest()->getParam('id'));
+
+    if($post_data = $this->getRequest()->getPost('testimonialData')) {
+      try {
+        $testimonial->addData($post_data);
+        $testimonial->save();
+
+        $this->_getSession()->addSuccess($this->__('Testimonial updated successfully!'));
+
+        return $this->_redirect('adminhtml/testimonials');
+      } catch (Exception $e) {
+        Mage::logException($e);
+        $this->_getSession()->addError($e->getMessage());
+      }
+    }
+
     Mage::register('current_testimonial', $testimonial);
     $editTestimonialBlock = $this->getLayout()->createBlock('testimonials/adminhtml_testimonial_edit');
     $this->loadLayout();
     $this->_addContent($editTestimonialBlock);
     $this->renderLayout();
+  }
+
+  public function deleteAction() {
+    $testimonial = Mage::getModel('testimonials/testimonial');
+    if($testimonial_id = $this->getRequest()->getParam('id', false)){
+      $testimonial->load($testimonial_id);
+      try {
+        $testimonial->delete();
+        $this->_getSession()->addSuccess($this->__('Testimonial deleted successfully!'));
+      } catch (Exception $e) {
+        Mage::logException($e);
+        $this->_getSession()->addError($e->getMessage());
+      }
+    } else {
+      $this->_getSession()->addError($this->__('Id not specified'));
+    }
+    return $this->_redirect('adminhtml/testimonials');
   }
 }
